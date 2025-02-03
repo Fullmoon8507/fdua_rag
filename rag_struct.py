e05d8da2abab24b6fa3d1af23824a048a3b96836
fullmoon <xxx@sample.com>
2025-02-04 00:20:06 +0900

    複数のPDFファイルを取り込むよう修正。

--- rag_struct.py
+++ rag_struct.py
@@ -2,33 +2,75 @@
 ChromaDBを用いてベクターデータベースを作成する。
 """
 import os
+import time
 
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import PyMuPDFLoader
-from langchain_community.vectorstores import Chroma
+# from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import AzureOpenAIEmbeddings
 
 load_dotenv()
 
+# load_pdf_list = [f"./documents/{i}.pdf" for i in range(1, 20)]
+load_pdf_list = [
+    "./documents/1.pdf",
+    "./documents/2.pdf",
+    "./documents/3.pdf",
+    "./documents/4.pdf",
+    "./documents/5.pdf",
+    "./documents/6.pdf",
+    "./documents/7.pdf",
+    "./documents/8.pdf",
+    "./documents/9.pdf",
+    "./documents/10.pdf",
+    "./documents/11.pdf",
+    "./documents/12.pdf",
+    "./documents/13.pdf",
+    "./documents/14.pdf",
+    "./documents/15.pdf",
+    "./documents/16.pdf",
+    "./documents/17.pdf",
+    "./documents/18.pdf",
+    "./documents/19.pdf",
+]
+
+# splitter
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+
+# Embedding
+embeddings = AzureOpenAIEmbeddings(
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    deployment=os.getenv("AZURE_OPENAI_EMBEDDINGS"),
+)
+
 if __name__ == "__main__":
 
-    # PDF読み込み
-    loader = PyMuPDFLoader("./documents/1.pdf")
-    data = loader.load()
+    # ベクターデータベースの保存先
+    PERSIST_DIRECTORY = "./chroma"
+    vectorstore = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings)
 
-    # テキスト分割
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-    chunks = text_splitter.split_documents(data)
+    for pdf_file in load_pdf_list:
 
-    # Embedding
-    embeddings = AzureOpenAIEmbeddings(
-        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-        api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
-        deployment=os.getenv("AZURE_OPENAI_EMBEDDINGS"),
-    )
+        print(f"{pdf_file}のEmbeddingを実行")
 
-    # ChromaDBに保存。また、永続化する。
-    vectorstore = Chroma.from_documents(chunks, embeddings, persist_directory="./chroma")
+        # PDF読み込み
+        loader = PyMuPDFLoader(pdf_file)
+        data = loader.load()
+
+        # テキスト分割
+        chunks = text_splitter.split_documents(data)
+
+        # ChromaDBに保存。また、永続化する。
+        vectorstore.add_documents(chunks)
+
+        # HTTPステータス500回避のため、Sleepを入れる。
+        time.sleep(30)
+
+    print("すべてのPDFをChromaDBに保存しました。")
+    for pdf in load_pdf_list:
+        print(pdf)
