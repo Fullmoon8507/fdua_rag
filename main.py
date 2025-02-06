e5e0644ba770869c763591f774bde2f790c29d87
fullmoon <xxx@sample.com>
2025-02-06 22:13:25 +0900

    query.csvから質問文(1問のみ)を読み込むように修正。

--- main.py
+++ main.py
@@ -12,6 +12,27 @@ from langchain.chains.retrieval_qa.base import RetrievalQA
 
 load_dotenv()
 
+# Model
+model = AzureChatOpenAI(
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    deployment_name=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
+    temperature=os.getenv("AZURE_OPENAI_EMPERATURE"),
+    max_tokens=os.getenv("AZURE_OPENAI_AX_TOKENS"),
+    top_p=os.getenv("AZURE_OPENAI_OP_P"),
+    frequency_penalty=os.getenv("AZURE_OPENAI_REQUENCY_PENALTY"),
+    presence_penalty=os.getenv("AZURE_OPENAI_RESENCE_PENALTY"),
+)
+
+# Embedding
+embeddings = AzureOpenAIEmbeddings(
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    deployment=os.getenv("AZURE_OPENAI_EMBEDDINGS"),
+)
+
 def read_problem_csv_file():
     """ メイン処理 """
     problem_list = []
@@ -23,28 +44,13 @@ def read_problem_csv_file():
 
         problem_list = [row[1] for row in reader]
 
+    return problem_list
+
 def main():
     """ メイン処理 """
-    # Model
-    model = AzureChatOpenAI(
-        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-        api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
-        deployment_name=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
-        temperature=os.getenv("AZURE_OPENAI_EMPERATURE"),
-        max_tokens=os.getenv("AZURE_OPENAI_AX_TOKENS"),
-        top_p=os.getenv("AZURE_OPENAI_OP_P"),
-        frequency_penalty=os.getenv("AZURE_OPENAI_REQUENCY_PENALTY"),
-        presence_penalty=os.getenv("AZURE_OPENAI_RESENCE_PENALTY"),
-    )
 
-    # Embedding
-    embeddings = AzureOpenAIEmbeddings(
-        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-        api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
-        deployment=os.getenv("AZURE_OPENAI_EMBEDDINGS"),
-    )
+    # queryファイル読み込み
+    problem_list = read_problem_csv_file()
 
     vectorstore = Chroma(persist_directory="./chroma", embedding_function=embeddings)
     retriever = vectorstore.as_retriever()
@@ -55,7 +61,7 @@ def main():
         return_source_documents=True,
     )
 
-    user_input = "株式会社キッツの取締役の報酬のうち株式報酬の割合は何％？"
+    user_input = problem_list[1]
     response = qa_chain.invoke(user_input)
     print(response["result"])
 
