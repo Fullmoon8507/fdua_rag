cf2a104f35a4cc839e6c8630bf4b2c7dfd3df1a8
fullmoon <xxx@sample.com>
2025-02-07 23:20:03 +0900

    リファクタリング。

--- main.py
+++ main.py
@@ -12,28 +12,8 @@ from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplat
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_chroma import Chroma
+
 load_dotenv()
-
-# Model
-model = AzureChatOpenAI(
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
-    deployment_name=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
-    temperature=os.getenv("AZURE_OPENAI_EMPERATURE"),
-    max_tokens=os.getenv("AZURE_OPENAI_AX_TOKENS"),
-    top_p=os.getenv("AZURE_OPENAI_OP_P"),
-    frequency_penalty=os.getenv("AZURE_OPENAI_REQUENCY_PENALTY"),
-    presence_penalty=os.getenv("AZURE_OPENAI_RESENCE_PENALTY"),
-)
-
-# Embedding
-embeddings = AzureOpenAIEmbeddings(
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
-    deployment=os.getenv("AZURE_OPENAI_EMBEDDINGS"),
-)
 
 def read_problem_csv_file():
     """ メイン処理 """
@@ -48,17 +28,54 @@ def read_problem_csv_file():
 
     return problem_list
 
-def main():
-    """ メイン処理 """
 
-    # queryファイル読み込み
-    problem_list = read_problem_csv_file()
+def make_chat_model():
+    """ Azure OpenAIのチャットモデルを生成 """
+
+    model = AzureChatOpenAI(
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+        deployment_name=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
+        temperature=os.getenv("AZURE_OPENAI_EMPERATURE"),
+        max_tokens=os.getenv("AZURE_OPENAI_AX_TOKENS"),
+        top_p=os.getenv("AZURE_OPENAI_OP_P"),
+        frequency_penalty=os.getenv("AZURE_OPENAI_REQUENCY_PENALTY"),
+        presence_penalty=os.getenv("AZURE_OPENAI_RESENCE_PENALTY"),
+    )
+
+    return model
+
+
+def make_embeddings():
+    """ Azure OpenAIのエンベディングを生成 """
+
+    embeddings = AzureOpenAIEmbeddings(
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+        deployment=os.getenv("AZURE_OPENAI_EMBEDDINGS"),
+    )
+
+    return embeddings
+
+
+def make_retriever():
+    """ リトリーバーを生成 """
+
+    # Embedding
+    embeddings = make_embeddings()
 
     # ベクターデータベースの読み込み
     vectorstore = Chroma(persist_directory="./chroma", embedding_function=embeddings)
     retriever = vectorstore.as_retriever()
 
-    # Prompt
+    return retriever
+
+
+def make_prompt():
+    """ プロンプトを生成 """
+
     human_message_template = HumanMessagePromptTemplate.from_template('''\
     文脈："""
     {context}
@@ -74,18 +91,45 @@ def main():
         ]
     )
 
+    return chat_prompt
+
+
+def make_output_parser():
+    """ パーサーを生成 """
+
+    return StrOutputParser()
+
+
+def main():
+    """ メイン処理 """
+
+    # queryファイル読み込み
+    problem_list = read_problem_csv_file()
+
+    # Retriever
+    retriever = make_retriever()
+
     # インプット（引数）
     input_dict = {
         "context": retriever,
         "question": RunnablePassthrough(),
     }
 
+    # Prompt
+    chat_prompt = make_prompt()
+
+    # Model
+    model = make_chat_model()
+
+    # Output Parser
+    output_parser = make_output_parser()
+
     # Chain
     chain = (
         input_dict
         | chat_prompt
         | model
-        | StrOutputParser()
+        | output_parser
     )
 
     response =  chain.invoke(problem_list[1])
